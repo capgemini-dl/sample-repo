188f44d13524a46ee5b3e0628cbe4f3bf71780aa
dongsoo.lee <dongsoo.lee@capgemini.com>
2025-03-21 21:15:40 -0400

    added travel recommendation v3

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,36 +1,57 @@
+const btnSearch = document.getElementById('btnSearch');
+const btnClear = document.getElementById('btnClear');
+
 function searchDestination() {
     const input = document.getElementById('destinationInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
 
+    console.log(input);
+    console.log(resultDiv.value);
     fetch('travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
-            const country = data.countries.find(item => item.name.toLowerCase() === input);
-
-            if (country) {
-                const cities = country.cities;
-
-                
-
-
-                const symptoms = condition.symptoms.join(', ');
-                const prevention = condition.prevention.join(', ');
-                const treatment = condition.treatment;
-
-
-                resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-                resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-                resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-                resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+            if (input === 'country' || input === 'countries') {
+                const idx = 0;
+                const dest = data.countries[idx].cities;
+                generateText(dest);
+                document.getElementById('destinationInput').value = '';
+            } else if (input === 'beach' || input === 'beaches') {
+                const dest = data.beaches; // list of 2
+                generateText(dest);
+                document.getElementById('destinationInput').value = '';
+            } else if (input === 'temple' || input === 'temples') {
+                const dest = data.temples; // list of 2
+                generateText(dest);
+                document.getElementById('destinationInput').value = '';
             } else {
                 resultDiv.innerHTML = 'Destination not found.';
+                document.getElementById('destinationInput').value = '';
             }
         })
         .catch(error => {
             console.error('Error:', error);
             resultDiv.innerHTML = 'An error occurred while fetching data.';
         });
+
+    function generateText(dest) {
+        if (dest) {
+            dest.forEach(destination => {
+                resultDiv.innerHTML += `<img src="${destination.imageUrl}" alt="hjh">`;
+                resultDiv.innerHTML += `<h2>${destination.name}</h2>`;
+                resultDiv.innerHTML += `<p>${destination.description}</p>`;
+            });
+        } else {
+            resultDiv.innerHTML = 'Destination not found.';
+        }
+    }
 }
+
+function resetResult() {
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+    document.getElementById('destinationInput').value = '';
+}
+
 btnSearch.addEventListener('click', searchDestination);
+btnClear.addEventListener('click', resetResult);
